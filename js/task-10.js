cccc1672d040260349c4f86fde2aa36e9c579593
Patryk Lubke <lubke.patryk@gmail.com>
2023-02-20 22:49:05 +0100

    task 10 done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesDiv = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  const amount = input.value;
+
+  if (amount > 0 && amount <= 100) {
+    createBoxes(amount);
+  }
+});
+
+destroyBtn.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    div.style.backgroundColor = getRandomHexColor();
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+
+    boxesDiv.append(div);
+
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
