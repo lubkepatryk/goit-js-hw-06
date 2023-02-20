c7c8df594365e8e9eeaca12daa85bdb2717ba2e5
Patryk Lubke <lubke.patryk@gmail.com>
2023-02-20 21:22:15 +0100

    task 2 done

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,11 @@
-const categoriesList = document.querySelectorAll('#categories .item');
+const categoriesList = document.querySelectorAll("#categories .item");
 const categoriesCount = categoriesList.length;
 console.log(`Number of categories: ${categoriesCount}`);
 
-categoriesList.forEach(category => {
-    const categoryName = category.querySelector('h2').textContent;
-    const categoryItems = category.querySelectorAll('li');
-    const categoryItemCount = categoryItems.length;
-    console.log(`Category: ${categoryName} 
+categoriesList.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryItems = category.querySelectorAll("li");
+  const categoryItemCount = categoryItems.length;
+  console.log(`Category: ${categoryName} 
 Elements: ${categoryItemCount}`);
-})
+});
